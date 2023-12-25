37195aa34df7704a8b89f2b45c3eb67f4c701acc
Prince Gupta <princegupta@gmail.com>
2023-12-25 12:19:24 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/about";
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>hello mern!!</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home/>}></Route>
+        <Route  path="/profile" element={<Profile/>}></Route>
+        <Route  path="/sign-in" element={<SignIn/>}></Route>
+        <Route  path="/sign-up" element={<SignUp/>}></Route>
+        <Route  path="/about" element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SignUp() {
+  return(
+    <div><h1>SignUp</h1></div>
+  )
+}
