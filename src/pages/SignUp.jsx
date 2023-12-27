33d1e946a6e94966b9f72007e4baa416ab4fc338
Prince Gupta <princegupta@gmail.com>
2023-12-27 12:44:42 +0530

    sign-up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return(
-    <div><h1>SignUp</h1></div>
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form className="flex flex-col gap-4" >
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" />
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" />
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" />
+        <button className="border bg-slate-700 rounded-lg p-3 uppercase text-white hover:opacity-90">sign up</button>
+      </form>
+      <div className="flex gap-2 my-4">
+        <p>Have an Account ?</p>
+        <Link to ={'/sign-in'}>
+          <span className="text-blue-700 ">Sing in</span>
+        </Link>
+      </div>
+    </div>
+    
   )
 }
